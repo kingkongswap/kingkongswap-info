275a179dae2012b4624da90b7e444e6bc12651d2
includeleec <includeleec@gmail.com>
2021-03-24 15:51:19 +0800

    fix typo

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -430,10 +430,10 @@ export const Search = ({ small = false }) => {
               : below410
               ? 'Search...'
               : below470
-              ? 'Search Uniswap...'
+              ? 'Search KingKongSwap...'
               : below700
               ? 'Search pairs and tokens...'
-              : 'Search Uniswap pairs and tokens...'
+              : 'Search KingKongSwap pairs and tokens...'
           }
           value={value}
           onChange={(e) => {
